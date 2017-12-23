6e05fea42d4f9c124789d6ddd6c68bdf6616de4b
Kanyango <kariukikanyango@gmail.com>
2017-12-23 21:06:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const path = require('path');
 const app = express();
 app.server = http.createServer(app);
 
-app.use(express.static(__dirname + '/src');
+app.use(express.static(__dirname + '/src'));
+        
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });
