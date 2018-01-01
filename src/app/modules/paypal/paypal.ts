b85e4438c26e4dc7e425be2089146467b70afe47
Kanyango <kariukikanyango@gmail.com>
2018-01-01 11:09:20 +0300

    Update paypal.ts

--- src/app/modules/paypal/paypal.ts
+++ src/app/modules/paypal/paypal.ts
@@ -1,44 +1,93 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { Headers, Http } from '@angular/http';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Car, Price } from '../utilities/car_model';
 
-<br>
-<br>
-<br>
-<div class="container">
-  <div class="row">
-    <div class="col-md-3"></div>
-    <div class="col-md-6">
-      <div class="card">
-        <div class="card-body">
-          <h3 class="card-title">Paypal Payment Card</h3>
-          <hr>
-          <h3>{{ car.model }}</h3>
-          <br>
-          <form [formGroup]="paypalForm" novalidate (ngSubmit)="onSubmit()" name="paypalForm">
-            <div class="md-form">
-                <i class="fa fa-calendar prefix grey-text"></i>
-                <input type="text" id="days" name="days" class="form-control"
-                mdbActive formControlName="days" >
-                <label for="days">Days</label>
-            </div>
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/switchMap';
 
-          <div class="form-group">
-            <label for="locate">Region</label>
-            <select class="form-control" formControlName="locate">
-              <option *ngFor="let one of car.pricing" value="{{ one.amount}}">
-                {{ one.desc }}
-              </option>
-            </select>
-          </div>
 
-          <br>
-          <button [disabled]="!paypalForm.valid" type="submit"
-           class="btn btn-success waves-light" mdbRippleRadius>submit</button>
-          </form>
-          <hr>
-          <h3>Total cost: <strong>{{ totals | currency:'KES' }}</strong></h3>
-          <br>
 
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+@Component({
+    selector: 'paypal-component',
+    templateUrl: './paypal.html'
+})
+
+export class PaypalComponent implements OnInit
+{
+    id: string;
+    str_tot: string;
+    car: Car;
+    totals: number = 0;
+    public carsUrl = 'https://kizusi.herokuapp.com/car/';
+    paypalForm: FormGroup;
+    constructor(
+                    private http: Http,
+                    private router: Router,
+                    private route: ActivatedRoute,
+                    private fb: FormBuilder
+              )
+               {
+                 this.createForm()
+               }
+    private loadExternalScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script')
+      scriptElement.src = scriptUrl
+      scriptElement.onload = resolve
+      document.body.appendChild(scriptElement)
+  })
+}
+ngOnInit(): void {
+  this.route.params.subscribe(params =>
+        {
+           this.id = params['id'];
+            console.log(this.id);
+            this.getCar(this.id);
+        })
+
+      }
+
+onSubmit()
+{
+    console.log(this.paypalForm.value);
+    let cost = parseInt(this.paypalForm.value.locate);
+    let days = parseInt(this.paypalForm.value.days)
+
+    this.totals = cost * days;
+    let new_tot =  this.totals;
+    this.str_tot = new_tot.toString();
+
+    this.router.navigate(['../confirmation', {'amount': this.str_tot, 'currency': 'USD'}])
+}
+
+
+getCar(id: string)
+{
+  const URL = `${this.carsUrl}${id}`;
+  return this.http.get(URL)
+              .toPromise()
+               .then(res => {
+                       this.car = res.json();
+                       console.log(res.json());
+
+                  })
+               .catch(this.handleError)
+
+}
+
+createForm()
+{
+  this.paypalForm = this.fb.group(
+      {
+          days  : ['', Validators.required],
+          locate: ['', Validators.required]
+      })
+}
+private handleError(error: any): Promise<any>
+ {
+console.error('An error occurred', error); // for demo purposes only
+return Promise.reject(error.message || error);
+}
+}
