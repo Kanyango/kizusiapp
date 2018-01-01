3f8bffb0fa0a5a3cb44c0c4a72a279fb97194d4d
Kanyango <kariukikanyango@gmail.com>
2018-01-01 11:55:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');	
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next(); 
+});
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
