306bdc92722e923ec92bd63738f8e78da2679064
Kanyango <kariukikanyango@gmail.com>
2018-01-24 10:08:57 +0300

    Update signup.ts

--- src/app/modules/signup/signup.ts
+++ src/app/modules/signup/signup.ts
@@ -1,1 +1,10 @@
 
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+    selector: 'sign-up-component',
+    templateUrl: './signup.html'
+})
+
+export class SignUpComponent {}
