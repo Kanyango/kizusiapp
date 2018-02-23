933433f6a29f59e1419a98685fc74f0eeb638492
Kanyango <kariukikanyango@gmail.com>
2018-02-23 12:17:09 +0300

    Update signup.ts

--- src/app/modules/signup/signup.ts
+++ src/app/modules/signup/signup.ts
@@ -1,6 +1,7 @@
 
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Headers, Http } from '@angular/htt
 
 @Component({
     selector: 'sign-up-component',
@@ -9,37 +10,28 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 
 export class SignUpComponent 
 {
-  (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = '//connect.facebook.net/en_US/sdk.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    public fbUrl = 'https://kizusi.herokuapp.com/oauth/facebook/';
+    private headers = new Headers({'Content-Type': 'application/json'});
 
+    constructor(private http: Http,
+            private router: Router,
+            private route: ActivatedRoute){}
+    
+    fbStrategy()
+    {
+       this.http.get(this.fbUrl)
+            .toPromise()
+            .then(res => {
+                        console.log(res.json());
+                        this.router.navigate(['../plan']);
 
-    window.fbAsyncInit = () => {
-        console.log("fbasyncinit")
-
-        FB.init({
-            appId            : '321878648322680',
-            autoLogAppEvents : true,
-            xfbml            : true,
-            version          : 'v2.10'
-        });
-        FB.AppEvents.logPageView();
-        // This is where we do most of our code dealing with the FB variable like adding an observer to check when the user signs in
-
-// ** ADD CODE TO NEXT STEP HERE **
-    };
-  FB.Event.subscribe('auth.statusChange', (response => {
-
-            if (response.status === 'connected') {
-                // use the response variable to get any information about the user and to see the tokens about the users session
-            }
-     function checkLoginState() {
-      FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-      });
+             })
+            .catch(this.handleError)
     }
+    private handleError(error: any): Promise<any>
+     {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+    }
+
 }
