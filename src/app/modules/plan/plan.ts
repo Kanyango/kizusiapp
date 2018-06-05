bc947e8314f42ea37eafe1bb647015ed0c3ced6e
Kanyango <kariukikanyango@gmail.com>
2018-06-05 08:25:48 +0300

    Update plan.ts

--- src/app/modules/plan/plan.ts
+++ src/app/modules/plan/plan.ts
@@ -9,43 +9,5 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 })
 
 export class PlanComponent {
-    items: Array<any> = [];
-    todos: Array<any> = [];
-
-    public planForm: FormGroup;
-    public toDoForm: FormGroup;
-    constructor(private _fb: FormBuilder)
-            {
-                this.makeForm();
-                this.todoForm();
-            }
-
-        makeForm()
-        {
-            this.planForm = this._fb.group(
-                {
-                    plan_item: ['', Validators.required]
-                })
-        }
-        todoForm()
-        {
-            this.toDoForm = this._fb.group(
-                {
-                    todo_item: ['', Validators.required]
-                })
-        }
-        onSubmit()
-        {
-            let one = this.planForm.value.plan_item;
-            this.items.push(one);
-            console.log(this.items);
-            this.planForm.value.plan_item = '';
-        }
-        ontodo()
-        {
-            let one = this.toDoForm.value.todo_item;
-            this.todos.push(one);
-            console.log(this.todos);
-            this.planForm.value.todo_item = '';
-        }
+    
 }
