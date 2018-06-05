a911e4e92a14566f96405b515447f03249306720
Kanyango <kariukikanyango@gmail.com>
2018-06-05 08:49:19 +0300

    Update flight.ts

--- src/app/modules/modules/flight/flight.ts
+++ src/app/modules/modules/flight/flight.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class FlightComponent {
-
+    
+    another: any = '';
+    firstDayOfWeek: number = 0;
     mode: string = 'date';
     departure: string;
     selectedOption = '';
