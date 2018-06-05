6cc1539e4ade1cd4e8c014aa5d490227fd998574
Kanyango <kariukikanyango@gmail.com>
2018-06-05 08:34:47 +0300

    Update flight.ts

--- src/app/modules/modules/flight/flight.ts
+++ src/app/modules/modules/flight/flight.ts
@@ -1,1 +1,18 @@
 
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'flight-component',
+    templateUrl: 'flight.html'
+})
+
+export class FlightComponent {
+
+    mode: string = 'date';
+    departure: string;
+    selectedOption = '';
+    arrival: string;
+    
+    trips = ['One Way', 'Round Trip', 'Multi City'];
+
+}
